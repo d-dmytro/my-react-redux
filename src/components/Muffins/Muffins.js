249711ae8fd839f6f982ff9715f23ee533e29b7e
Dmytro <d-dmytro@users.noreply.github.com>
2020-01-26 14:44:41 +0200

    Read the muffins array from the state

--- src/components/Muffins/Muffins.js
+++ src/components/Muffins/Muffins.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectMuffinsArray } from '../../redux/selectors';
 
 const Muffins = () => {
+  const muffins = useSelector(selectMuffinsArray);
+
   return (
     <ul>
-      <li>Chocolate chip muffin</li>
-      <li>Blueberry muffin</li>
-      <li>Banana muffin</li>
+      {muffins.map(muffin => {
+        return <li key={muffin.id}>{muffin.name}</li>;
+      })}
     </ul>
   );
 };
