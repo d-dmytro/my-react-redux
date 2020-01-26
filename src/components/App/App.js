088ff643896ae1fbcbc55889369c8d30c53d5306
Dmytro <d-dmytro@users.noreply.github.com>
2020-01-26 13:22:26 +0200

    Removed extra code and hard-coded a list of muffins

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Muffins from '../Muffins';
+
+function App() {
+  return (
+    <div className="App">
+      <Muffins />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Muffins/Muffins.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Muffins = () => {
+  return (
+    <ul>
+      <li>Chocolate chip muffin</li>
+      <li>Blueberry muffin</li>
+      <li>Banana muffin</li>
+    </ul>
+  );
+};
+
+export default Muffins;
